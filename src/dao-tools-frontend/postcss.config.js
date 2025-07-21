996d9da55cb923b8340862b6d14948a193947fb3
0xPoggufanz <mfaiq1205@gmail.com>
2025-07-21 14:08:45 +0700

    fix: next/font | 21/07 (#1)

--- src/dao-tools-frontend/postcss.config.js
+++ src/dao-tools-frontend/postcss.config.js
@@ -1,7 +1,8 @@
+// postcss.config.js
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer'
+  ]
+}
